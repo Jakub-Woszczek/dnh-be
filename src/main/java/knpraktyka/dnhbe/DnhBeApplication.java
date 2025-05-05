775c26184c4462fc6a8bbfb121e8ca5fcc57bd89
Jakub Woszczek <icefreestyle2000@gmail.com>
2025-05-05 13:45:06 +0200

    Project setup

---
+++ src/main/java/knpraktyka/dnhbe/DnhBeApplication.java
@@ -0,0 +1,11 @@
+package knpraktyka.dnhbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnhBeApplication {
+      public static void main(String[] args) {
+            SpringApplication.run(DnhBeApplication.class, args);
+      }
+}
